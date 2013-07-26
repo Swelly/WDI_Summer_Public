dcc2b05164858b45c1abc06752f0eafd9d6982ec
Wesley Carr <surfswell17@gmail.com>
2013-07-26 11:26:02 -0400

    Ruby and javascript fizzbuzz

--- assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
+++ assignments/fizzbuzz/fizzbuzz_ruby_bonus.rb
@@ -1,1 +1,22 @@
-# Done in JS? Now do it in Ruby. 
+# Done in JS? Now do it in Ruby.
+
+for num in 1..100 do
+
+  fizzBuzz = false
+  if num % 3 == 0
+    print "Fizz"
+    fizzBuzz = true
+  end
+
+  if num % 5 == 0
+    print "Buzz"
+    fizzBuzz = true
+  end
+
+  if fizzBuzz == false
+    print num
+  end
+
+  puts
+
+end
--- assignments/fizzbuzz/js/fizzbuzz.js
+++ assignments/fizzbuzz/js/fizzbuzz.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// underscore to create a range
+var range = _.range(1,101);
+
+// creating fizzList with _.map
+// makes fizzList an array with the function iteration
+var fizzList = _.map(list, function(number){
+  if (number % 3 === 0 && number % 5 === 0) {
+    return ('Fizzbuzz');
+  } else if (number % 3 === 0){
+    return ('Fizz');
+  } else if (number % 5 === 0){
+    return  ('Buzz');
+  }
+});
