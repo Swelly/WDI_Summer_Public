73c288b2782f42be42a809bcd305ad3ce82b5bac
Wesley Carr <surfswell17@gmail.com>
2013-07-30 13:50:01 -0400

    jquery validation

--- assignments/atm-student_pureJS/atm.js
+++ assignments/atm-student_pureJS/atm.js
@@ -3,22 +3,22 @@ var savingsBalance = 0;
 
 window.onload = function(){
 
-  document.getElementById('checking-deposit').onclick = function getChecking(event){
-    var amount = document.getElementById('checking-amount').value;
+  $('input#checkingDeposit').onclick = function getChecking(event){
+    var amount = $('input#checkingAmount').val();
     amount = parseInt(amount);
     checkingBalance = amount + checkingBalance;
     updateDisplay();
   };
 
-  document.getElementById('savings-deposit').onclick = function(event){
-    var amount = document.getElementById('savings-amount').value;
+  $('input#savings-deposit').onclick = function(event){
+    var amount = $('input#savings-amount').val();
     amount = parseInt(amount);
     savingsBalance = amount + savingsBalance;
     updateDisplay();
   };
 
-  document.getElementById('checking-withdraw').onclick = function(event){
-    var amount = document.getElementById('checking-amount').value;
+  $('input#checking-withdraw').onclick = function(event){
+    var amount = $('input#checking-amount').val();
     amount = parseInt(amount);
     balances = withdrawFunds(amount, checkingBalance, savingsBalance);
     checkingBalance = balances[0];
@@ -26,8 +26,8 @@ window.onload = function(){
     updateDisplay();
   };
 
-  document.getElementById('savings-withdraw').onclick = function(event){
-    var amount = document.getElementById('savings-amount').value;
+  $('input#savings-withdraw').onclick = function(event){
+    var amount = $('input#savings-amount').val();
     amount = parseInt(amount);
     balances = withdrawFunds(amount, savingsBalance, checkingBalance);
     savingsBalance = balances[0];
@@ -51,7 +51,7 @@ function withdrawFunds(amount, primary, secondary) {
 
 function updateDisplay() {
 
-  var element = document.getElementById('checking-balance');
+  var element = $('div#checking-balance')[0];
 
   if (checkingBalance <= 0) {
     element.classList.add('zero');
@@ -59,15 +59,15 @@ function updateDisplay() {
     element.classList.remove('zero');
   }
 
-  var element2 = document.getElementById('savings-balance');
+  var element2 = $('div#savings-balance.balance')[0];
 
   if (savingsBalance <= 0) {
     element2.classList.add('zero');
   } else {
     element2.classList.remove('zero');
   }
-  document.getElementById('checking-balance').innerHTML = '$' + checkingBalance;
-  document.getElementById('savings-balance').innerHTML = '$' + savingsBalance;
-  document.getElementById('checking-amount').value = '';
-  document.getElementById('savings-amount').value = '';
+  $('div#checking-balance')[0].innerHTML = '$' + checkingBalance;
+  $('div#savings-balance')[0].innerHTML = '$' + savingsBalance;
+  $('div#checking-amount')[0].val() = '';
+  $('div#savings-amount')[0].val() = '';
 }
